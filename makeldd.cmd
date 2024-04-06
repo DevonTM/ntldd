8193b515a898b7394dbf4bbfa840ccc00a2cdb4d
Irfan Ammar Afif <ipangcheater@gmail.com>
2024-04-07 03:20:34 +0700

    Support MSVC build

--- makeldd.cmd
+++ makeldd.cmd
@@ -1,4 +1,4 @@
 @echo off
-gcc -fno-common -g -O3 -Wall -D__USE_MINGW_ANSI_STDIO=1 -DWINVER=0x0A00 -D_WIN32_WINNT=0x0A00 -c libntldd.c -o libntldd.o
-ar rs libntldd.a libntldd.o
-gcc -fno-common -g -O3 -Wall -D__USE_MINGW_ANSI_STDIO=1 ntldd.c -L. -lntldd -limagehlp -o ntldd.exe
+cl.exe /c /O2 /W4 /DWINVER=0x0A00 /D_WIN32_WINNT=0x0A00 libntldd.c /Fo:libntldd.obj
+lib libntldd.obj /OUT:ntldd.lib
+cl.exe /O2 /W4 ntldd.c ntldd.lib imagehlp.lib /Fe:ntldd.exe
